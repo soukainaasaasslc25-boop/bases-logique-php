0b9ffeb406b6445a09b90996035c60a6497bc2f4
soukainaasaasslc25-boop <soukaina.asaas.slc25@gmail.com>
2026-01-20 09:55:37 +0000

    variables

---
+++ variables.php
@@ -0,0 +1,18 @@
+<?php
+$nom = "Alice";
+$age = 25;
+$prix = 19.99;
+$estConnecte = true;
+
+echo "Nom : " . $nom . "<br>";
+echo "Âge : " . $age . "<br>";
+echo "Prix : " . $prix . " €<br>";
+echo "Connecté : " . ($estConnecte ? "Oui" : "Non") . "<br>";
+
+var_dump($nom);
+$nombre = "10";
+$nombreInt = (int)$nombre;
+echo "Conversion : " . $nombreInt;
+
+
+
