c92992646c63b53fdbd6fa18fd02ba6fefcc0afe
soukainaasaasslc25-boop <soukaina.asaas.slc25@gmail.com>
2026-01-20 10:08:41 +0000

    start autoformation

---
+++ chaines.php
@@ -0,0 +1,22 @@
+<?php
+$phrase = "Hello PHP";
+echo "Longueur : " . strlen($phrase) . "<br>";
+echo "Majuscules : " . strtoupper($phrase) . "<br>";
+echo "Minuscules : " . strtolower($phrase) . "<br>";
+
+
+// Rechercher et remplacer
+echo "Position de PHP : " . strpos($phrase, "PHP") . "<br>";
+$nouvellePhrase = str_replace("PHP", "le monde", $phrase);
+echo "Remplacé : " . $nouvellePhrase . "<br>";
+
+// Découper et assembler
+$liste = "HTML,CSS,JavaScript,PHP";
+$techs = explode(",", $liste);
+echo "Technologies : " . implode(" | ", $techs);
+
+// Étape 4 : Nettoyer une chaîne
+$texte = "   Bonjour   ";
+echo "Texte nettoyé : '" . trim($texte) . "'";
+
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include 'header.php';
+?>
+<main>
+    <h2>Page d'accueil</h2>
+    <p>Contenu de la page principale.</p>
+</main>
+<?php
+include 'footer.php';
